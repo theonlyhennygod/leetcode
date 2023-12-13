130bf38991c359bcf250ec0f1e01da382297e913
Argenis <144828210+theonlyhennygod@users.noreply.github.com>
2023-12-13 07:38:22 -0500

    Add files via upload

---
+++ leetcode-practice-4.js
@@ -0,0 +1,44 @@
+/*
+
+2413. Smallest Even Multiple
+Easy
+Topics
+Companies
+Hint
+Given a positive integer n, return the smallest positive integer that is a multiple of both 2 and n.
+
+
+Example 1:
+
+Input: n = 5
+Output: 10
+Explanation: The smallest multiple of both 5 and 2 is 10.
+Example 2:
+
+Input: n = 6
+Output: 6
+Explanation: The smallest multiple of both 6 and 2 is 6. Note that a number is a multiple of itself.
+
+
+Constraints:
+
+1 <= n <= 150
+
+*/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var smallestEvenMultiple = function(n) {
+
+    // the smallest multiple of 2 
+
+    // if the n is odd, then we multiply the n by 2
+
+    // if it's even, we multiply n by 1 
+
+    // check if n is greater than or equal to 0 : edge case
+
+    // return n = Int
+};
